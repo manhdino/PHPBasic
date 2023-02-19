6ef0027892a7eb0d21958d0a917516b214378ab7
manh dino <manhnguyen1238@gmail.com>
2023-02-19 13:47:38 +0700

    print_out_example

---
+++ print_out_example.php
@@ -0,0 +1,22 @@
+ <!-- <?php
+  echo "Hello world\n";
+  echo "pi = ",3.14, ', x = ',4;
+   print "hello"; //correct
+  // print 'Hello',"world";// error vì chỉ in ra 1 xau 
+  print_r(["John","Hoang","Tony"]);
+  var_export(3); 
+?>  -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+   <h1><?php echo "Halo from html code by using echo";?></h1>
+   <h1><?= "Halo from html code by using = ";?></h1>
+</body>
+</html>
